7c34cf762f751e7ca3bf0e68b8f09d4f437b1343
chandana <chandanageddam11@gmail.com>
2018-06-14 16:18:41 -0400

    Added data provider

--- NewApplication/src/test/java/SeleniumLearnings/NewApplication/firsttest.java
+++ NewApplication/src/test/java/SeleniumLearnings/NewApplication/firsttest.java
@@ -4,13 +4,14 @@ import java.io.IOException;
 import SeleniumLearnings.NewApplication.Homepage;
 
 import org.openqa.selenium.WebDriver;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class firsttest extends BaseClass {
 
 	
-	@Test
-	public void login() throws IOException
+	@Test(dataProvider="getData")
+	public void login(String username,String password) throws IOException
 	
 	{
 		//Initialize desired browser
@@ -22,12 +23,29 @@ public class firsttest extends BaseClass {
 		Homepage page1= new Homepage(driver);
 		
 		//Send values to Facebook username field
-		page1.getusername().sendKeys("chandanag29@gmail.com");
+		page1.getusername().sendKeys(username);
 		//Send values to facebook password field
-		page1.getpassword().sendKeys("5512207712");
+		page1.getpassword().sendKeys(password);
 		//Click on Login push button
 		page1.getLogin().submit();
 		
+		
+		
+	}
+	
+	@DataProvider
+	
+	public Object[][] getData()
+	{
+		Object[][] data = new Object[1][1];
+		data[0][0] = "chandanag29@gmail.com";
+		data[0][1] = "5512207712";
+		data[1][0] = "chandanageddam1@gmail.com";
+		data[1][1] = "abcd";
+		
+		return data;
+		
+				
 		
 		
 	}
